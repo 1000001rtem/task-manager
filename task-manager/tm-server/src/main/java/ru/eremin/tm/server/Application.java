66470c6a148672693586cac16fa17193ec08fc8c
1000001rtem <1000001rtem@gmail.com>
2019-04-18 19:03:15 +0300

    split app into modulus, start endpoints

---
+++ task-manager/tm-server/src/main/java/ru/eremin/tm/server/Application.java
@@ -0,0 +1,22 @@
+package ru.eremin.tm.server;
+
+import org.jetbrains.annotations.NotNull;
+import ru.eremin.tm.server.bootstrap.Bootstrap;
+import ru.eremin.tm.server.bootstrap.ServiceLocator;
+import ru.eremin.tm.server.endpoint.TaskEndpoint;
+
+import javax.xml.ws.Endpoint;
+
+/**
+ * Hello world!
+ */
+public class Application {
+
+    private static final Class[] CLASSES = {
+            TaskEndpoint.class
+    };
+    public static void main(String[] args) {
+        @NotNull final ServiceLocator locator = new Bootstrap();
+        locator.init(CLASSES);
+    }
+}
