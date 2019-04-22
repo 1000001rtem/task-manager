cd369344cee776604a4e289bbcc860608b18b7a7
1000001rtem <1000001rtem@gmail.com>
2019-04-22 16:40:07 +0300

    add doc plugin and codgen plugin

--- task-manager/tm-client/src/test/java/ru/eremin/tm/client/AppTest.java
+++ task-manager/tm-client/src/test/java/ru/eremin/tm/client/AppTest.java
@@ -3,6 +3,10 @@ package ru.eremin.tm.client;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import ru.eremin.tm.server.endpoint.AuthorizationEndpoint;
+import ru.eremin.tm.server.endpoint.AuthorizationEndpointService;
+import ru.eremin.tm.server.endpoint.Login;
+import ru.eremin.tm.server.endpoint.LoginResponse;
 
 /**
  * Unit test for simple App.
@@ -16,5 +20,11 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void test(){
+        AuthorizationEndpointService authorizationEndpoint = new AuthorizationEndpointService();
+        System.out.println(authorizationEndpoint.getAuthorizationEndpointPort().login("user", "pass").getUserRole());
     }
 }
