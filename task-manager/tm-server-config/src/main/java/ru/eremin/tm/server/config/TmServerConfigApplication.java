8e90b0d3ac93ed5de858833c5afc51a578eae058
1000001rtem <1000001rtem@gmail.com>
2019-06-20 13:27:16 +0300

    add config server

---
+++ task-manager/tm-server-config/src/main/java/ru/eremin/tm/server/config/TmServerConfigApplication.java
@@ -0,0 +1,13 @@
+package ru.eremin.tm.server.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmServerConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TmServerConfigApplication.class, args);
+    }
+
+}
