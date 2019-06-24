49f56c1551db0b75a92171169b379e9a1340e3b1
1000001rtem <1000001rtem@gmail.com>
2019-06-24 07:13:38 +0300

    add PROXY

---
+++ task-manager/tm-proxy-server/src/test/java/ru/eremin/tm/proxy/TmProxyServerApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.eremin.tm.proxy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TmProxyServerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
